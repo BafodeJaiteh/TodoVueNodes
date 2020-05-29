ac09a21f20eb142882da1002e0d1853c3d3d1275
bafode <bafodejaiteh2@gmail.com>
2020-05-29 23:50:27 +0200

    import bulma

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    TodoList
   }
 };
 </script>
